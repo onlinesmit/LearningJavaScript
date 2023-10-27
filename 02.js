47a0c27f8d372776377bbe3979c4874f63bb9b20
onlinesmit <onlinesmitg@gmail.com>
2023-10-28 00:35:18 +0530

    after some changes

--- 02.js
+++ 02.js
@@ -1,6 +1,27 @@
+"use strict";
 //intro to variable
 
 //variable can store same information
 
-var firstName = "Smit";
+// //declaration
+// var firstName = "Smit";
+// //use of variable
+// console.log(firstName);
+
+//  var firstname = "Kumar";
+
+// console.log(firstname);
+
+
+// we can also write like this
+
+var firstName  ;
+//use of variable
+firstName = 'Smit'
 console.log(firstName);
+
+ firstName = "Kumar";
+
+console.log(firstName);
+
+//rule of decalre variable
